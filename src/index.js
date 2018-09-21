8759f91dd1c56bb950cc44492e8c3156113b5af0
Misha Maniuk <mikhail.maniuk@gmail.com>
2018-09-22 02:25:09 +0300

    love triangle task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for (var i = 0; i < preferences.length; i++ ) {
+    var ang1 = preferences[i];
+    var ang2 = preferences[ang1 - 1];
+    var ang3 = preferences[ang2 - 1];
+
+    if (ang1 != ang2 && ang2 != ang3 && ang3 != ang1 && i + 1 === ang3) {
+      count++
+    }
+  }
+  return count / 3;
 };
